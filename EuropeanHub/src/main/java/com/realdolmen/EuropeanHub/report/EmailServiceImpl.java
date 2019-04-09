cd53ee3717ccc899cb7475f180e3c4a19a383266
Sander Beazar <Sander.Beazar@realdolmen.com>
2019-04-09 09:59:36 +0200

    catch exception bij connecterror

--- EuropeanHub/src/main/java/com/realdolmen/EuropeanHub/report/EmailServiceImpl.java
+++ EuropeanHub/src/main/java/com/realdolmen/EuropeanHub/report/EmailServiceImpl.java
@@ -6,6 +6,7 @@
 package com.realdolmen.EuropeanHub.report;
 
 import java.io.File;
+import java.net.ConnectException;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,8 +22,8 @@ public class EmailServiceImpl {
     public JavaMailSender emailSender;
 
    // @PostConstruct
-    public void sendMessageWithAttachment(String to, String subject, String text, String pathToAttachment) throws MessagingException {
-        MimeMessage message = emailSender.createMimeMessage();
+    public void sendMessageWithAttachment(String to, String subject, String text, String pathToAttachment) throws MessagingException, ConnectException {
+            MimeMessage message = emailSender.createMimeMessage();
 
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
@@ -34,7 +35,7 @@ public class EmailServiceImpl {
      = new FileSystemResource(new File(pathToAttachment));
    helper.addAttachment("aanrijdingsformulier.pdf", file);
          
-        emailSender.send(message);
+        emailSender.send(message);        
     }
 
 }
